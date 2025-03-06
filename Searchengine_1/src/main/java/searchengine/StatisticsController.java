2d73384baca1519c92575ce23c4a8105f2a46c63
Solopenko2005 <solopenko2005@gmail.com>
2025-03-06 21:57:09 +0300

    Add files via upload

---
+++ Searchengine_1/src/main/java/searchengine/StatisticsController.java
@@ -0,0 +1,23 @@
+package searchengine.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import searchengine.dto.statistics.StatisticsResponse;
+import searchengine.services.StatisticsService;
+
+@RestController
+@RequestMapping("/api")
+public class StatisticsController {
+
+    private final StatisticsService statisticsService;
+
+    public StatisticsController(StatisticsService statisticsService) {
+        this.statisticsService = statisticsService;
+    }
+
+    @GetMapping("/statistics")
+    public StatisticsResponse getStatistics() {
+        return statisticsService.getStatistics();
+    }
+}
